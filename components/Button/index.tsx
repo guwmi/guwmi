1aec7a9a24978a9ac649559f866c18827b4eb7e3
Rustin Dodd <rustindodd@gmail.com>
2025-04-21 14:57:07 -0500

    adds basic button component styles

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -4,12 +4,23 @@ import React, { PropsWithChildren, useMemo } from 'react';
 // component type
 interface ComponentProps extends PropsWithChildren {
   size?: 'sm' | 'md' | 'lg';
+  color?: 'primary' | 'primary-light' | 'secondary' | 'secondary-light' | 'black' | 'white' | 'gray';
+  kind?: 'fill' | 'outline' | 'ghost';
+  style?: 'square' | 'round' | 'pill';
+  className?: string;
 }
 
 export default function Button(props: ComponentProps) {
 
-  const { children, size = 'md' } = props;
-  const classes = useMemo(() => `guwmi-btn ${size}`, []);
+  const {
+    children,
+    size = 'md',
+    color = 'primary',
+    kind = 'fill',
+    style = 'round',
+    className
+  } = props;
+  const classes = useMemo(() => `guwmi-btn ${size} ${color} ${kind} ${style}${className ? className : ''}`, []);
 
   return (
     <button className={classes}>{children}</button>
--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+// import library styles sheet
 import './styles/dist/index.css';
+
+// import button components
 import Button from './components/Button';
-export { Button };
+
+// export all components
+export {
+  Button
+};
