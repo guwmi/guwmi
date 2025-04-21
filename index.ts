6d969a90f0a7221bb067c7938d63ed34c69a839d
Rustin Dodd <rustindodd@gmail.com>
2025-04-21 15:14:00 -0500

    fixes sass watch to dump everything into dist dir

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // import library styles sheet
-import './src/styles/dist/index.css';
+import './dist/styles/index.css';
 
 // import button components
 import Button from './src/components/Button';
