70d20eb6d03713fa6a2566eb1a098a59bea58b59
Rustin Dodd <rustindodd@gmail.com>
2025-05-21 15:15:35 -0500

    adds tests for icon button component

--- src/__tests__/IconButton.jest.tsx
+++ src/__tests__/IconButton.jest.tsx
@@ -2,13 +2,18 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import component to test
-import Button from '../components/Button/Button';
+import IconButton from '@components/IconButton/IconButton';
+import Icon from '@components/Icon/Icon';
 
-describe('Button', () => {
+describe('Icon Button', () => {
 
   test('renders with children', () => {
 
-    render(<Button>Hello Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria">
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toBeInTheDocument();
@@ -17,26 +22,25 @@ describe('Button', () => {
   test('calls onClick handler when clicked', () => {
 
     const handleClick = jest.fn();
-    render(<Button onClick={handleClick}>Click Me</Button>);
+    render(
+      <IconButton ariaLabel="test aria" onClick={handleClick}>
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 
-  test('renders as link with correct href', () => {
-
-    render(<Button href="https://www.google.com">Link</Button>);
-
-    const link = screen.getByText('Link');
-    expect(link.tagName).toBe('A');
-    expect(link).toHaveAttribute('href', 'https://www.google.com');
-  });
-
   test('does not call onClick handler when disabled', () => {
 
     const handleClick = jest.fn();
-    render(<Button onClick={handleClick} disabled>Disabled Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria" onClick={handleClick} disabled>
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
@@ -45,23 +49,23 @@ describe('Button', () => {
 
   test('is disabled when disabled prop is true', () => {
 
-    render(<Button disabled>Click Me</Button>);
+    render(
+      <IconButton ariaLabel="test aria" disabled>
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toBeDisabled();
   });
 
-  test('renders with target attribute', () => {
-
-    render(<Button target="_blank" href="https://www.google.com">Target Link</Button>);
-
-    const link = screen.getByText('Target Link');
-    expect(link).toHaveAttribute('target', '_blank');
-  });
-
   test('applies custom className', () => {
 
-    render(<Button className="extra-class">Styled Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria" className="extra-class">
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toHaveClass('extra-class');
@@ -69,7 +73,11 @@ describe('Button', () => {
 
   test('renders with correct defaults', () => {
 
-    render(<Button>Default Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria">
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toHaveClass(/round/);
@@ -80,7 +88,11 @@ describe('Button', () => {
 
   test('renders with override size', () => {
 
-    render(<Button size="sm">Small Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria" size="sm">
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toHaveClass(/sm/);
@@ -88,7 +100,11 @@ describe('Button', () => {
 
   test('renders with override color', () => {
 
-    render(<Button color="danger">Danger Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria" color="danger">
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toHaveClass(/danger/);
@@ -96,7 +112,11 @@ describe('Button', () => {
 
   test('renders with override variant', () => {
 
-    render(<Button variant="fill">Fill Button</Button>);
+    render(
+      <IconButton ariaLabel="test aria" variant="fill">
+        <Icon name="menu" />
+      </IconButton>
+    );
 
     const button = screen.getByRole('button');
     expect(button).toHaveClass(/fill/);
@@ -104,9 +124,13 @@ describe('Button', () => {
 
   test('renders with override theme', () => {
 
-    render(<Button theme="pill">Pill Button</Button>);
-
+    render(
+      <IconButton ariaLabel="test aria" theme="circle">
+        <Icon name="menu" />
+      </IconButton>
+    );
+    
     const button = screen.getByRole('button');
-    expect(button).toHaveClass(/pill/);
+    expect(button).toHaveClass(/circle/);
   });
 });
