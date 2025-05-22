ffaa6b6442fe26d409a208593995b11fcee52a3c
Rustin Dodd <rustindodd@gmail.com>
2025-05-22 11:02:20 -0500

    adds tests for table component

--- src/components/Table/TableContext.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Table context for the table component *************************************************************
- */
-
-import { createContext } from 'react';
-const TableContext = createContext(null);
-export default TableContext;
