7d7b2e06fe3d8f53d4b264d7573e97802e8f3ba8
Rustin Dodd <rustindodd@gmail.com>
2025-05-28 11:29:00 -0500

    adds jest test for useCloseOutClick hook

---
+++ src/__tests__/hooks/useCloseOutClick.jest.tsx
@@ -0,0 +1,28 @@
+// import library functionality
+import { createRef } from 'react';
+import { render, renderHook, fireEvent, screen } from '@testing-library/react';
+
+// import hook
+import useCloseOutClick from '@hooks/useCloseOutClick';
+
+describe('useCloseOutClick Hook', () => {
+
+  test('calls onClose when clicked outside ref', () => {
+
+    render(
+      <>
+        <div id="not-ref" data-testid="not-ref">Not the ref</div>
+        <div id="ref">The ref</div>
+      </>
+    );
+    const mockRef = { current: document.getElementById('ref') };
+    const mockClose = jest.fn();
+    const { result, rerender } = renderHook((props) => useCloseOutClick(props.open, props.onClose, props.ref), {initialProps: {open: true, onClose: mockClose, ref: mockRef}});
+    const notRef = screen.getByTestId('not-ref');
+    fireEvent.click(notRef);
+    expect(mockClose).toHaveBeenCalledTimes(1);
+    rerender({open: false, onClose: mockClose, ref: mockRef});
+    fireEvent.click(notRef);
+    expect(mockClose).toHaveBeenCalledTimes(1);
+  })
+})
