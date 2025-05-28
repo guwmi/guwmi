1c944e817da0daecfc8e7ce84b04e432c4d4fbe6
Rustin Dodd <rustindodd@gmail.com>
2025-05-28 10:51:42 -0500

    adds jest test for usePreventScroll hook

---
+++ src/__tests__/hooks/usePreventScroll.jest.tsx
@@ -0,0 +1,23 @@
+// import library functionality
+import { render, renderHook } from '@testing-library/react';
+
+// import hook
+import usePreventScroll from '@hooks/usePreventScroll';
+
+// import component for render
+import Button from '@components/Button/Button';
+
+describe('usePreventScroll Hook', () => {
+
+  test('adds the correct styles to the body element', () => {
+    const { result, rerender } = renderHook((props) => usePreventScroll(props.open, props.enabled), {initialProps: {open: false, enabled: true}});
+    render(
+      <Button>Test button</Button>
+    )
+    expect(document.body).not.toHaveStyle('height: 100%');
+    expect(document.body).not.toHaveStyle('overflow: hidden');
+    rerender({open: true, enabled: true});
+    expect(document.body).toHaveStyle('height: 100%');
+    expect(document.body).toHaveStyle('overflow: hidden');
+  })
+})
