2df859954121758122189b74115385b233c5232c
Rustin Dodd <rustindodd@gmail.com>
2025-05-29 09:04:44 -0500

    adds to useAnimation jest tests

--- src/__tests__/hooks/useAnimation.jest.tsx
+++ src/__tests__/hooks/useAnimation.jest.tsx
@@ -1,12 +1,12 @@
 // import library functionality
-import { render, renderHook, fireEvent, screen } from '@testing-library/react';
+import { render, renderHook, screen } from '@testing-library/react';
 
 // import hook
 import useAnimation from '@hooks/useAnimation';
 
 describe('useAnimation Hook', () => {
 
-  test('adds and removes class as expected', async () => {
+  test('adds and removes class and return visibility as expected', async () => {
 
     render(
       <>
@@ -17,8 +17,10 @@ describe('useAnimation Hook', () => {
     const { result, rerender } = renderHook((props) => useAnimation(props.open, props.class, props.ref), {initialProps: {open: false, class: 'test-class', ref: mockRef}});
     const div = screen.getByTestId('ref');
     expect(div).not.toHaveClass('test-class');
+    expect(result.current.isVisible).toBe(false);
     rerender({open: true, class: 'test-class', ref: mockRef});
     await new Promise(resolve => setTimeout(resolve, 50));
     expect(div).toHaveClass('test-class');
+    expect(result.current.isVisible).toBe(true);
   })
 })
