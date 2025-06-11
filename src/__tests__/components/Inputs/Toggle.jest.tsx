11d90c1eb7af9176cfef85ef803e905e05673c94
Rustin Dodd <rustindodd@gmail.com>
2025-06-11 13:40:12 -0500

    adds jest test for Togle component

---
+++ src/__tests__/components/Inputs/Toggle.jest.tsx
@@ -0,0 +1,83 @@
+// import library functionality
+import { render, screen } from '@testing-library/react';
+import userEvent, { UserEvent } from '@testing-library/user-event';
+
+// import component
+import Toggle from '../../../components/Inputs/Toggle/Toggle';
+
+describe('RadioGroup', () => {
+
+  let user: UserEvent;
+  beforeEach(() => {
+    user = userEvent.setup();
+    document.body.innerHTML = '';
+  });
+
+  test('renders with defaults', () => {
+
+    render (<Toggle label="Test toggle" data-testid="guwmi-toggle" />);
+
+    const inputContainer = screen.getByTestId('guwmi-toggle');
+    const input = inputContainer.querySelector('input');
+    const label = screen.getByText("Test toggle");
+    expect(inputContainer).toBeInTheDocument();
+    expect(label).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+  });
+
+  test('renders with override values', () => {
+
+    render(
+      <Toggle
+        label="Test toggle"
+        id="test-id"
+        name="test-name"
+        disabled
+        value="test-value"
+        readOnly
+        className="override-class"
+        data-testid="guwmi-toggle"
+      />
+    )
+
+    const inputContainer = screen.getByTestId('guwmi-toggle');
+    const input = inputContainer.querySelector('input');
+    const label = screen.getByText("Test toggle");
+    expect(inputContainer).toBeInTheDocument();
+    expect(inputContainer).toHaveClass('override-class');
+    expect(inputContainer).toHaveClass('disabled');
+    expect(label).toBeInTheDocument();
+    expect(input).toHaveProperty('disabled');
+    expect(input).toHaveProperty('readOnly');
+    expect(input).toHaveAttribute('name', 'test-name');
+    expect(input).toHaveAttribute('id', 'test-id');
+    expect(input).toHaveAttribute('value', 'test-value');
+  });
+
+  test('correctly handles user events', async () => {
+
+    const changeHandler = jest.fn();
+    const blurHandler = jest.fn();
+    const focusHandler = jest.fn();
+
+    render(
+      <Toggle
+        label="Test toggle"
+        onChange={changeHandler}
+        onBlur={blurHandler}
+        onFocus={focusHandler}
+        data-testid="guwmi-toggle"
+      />
+    )
+
+    const inputContainer = screen.getByTestId('guwmi-toggle');
+    const input = inputContainer.querySelector('input');
+    input.focus();
+    await user.click(input);
+    input.blur();
+    expect(focusHandler).toHaveBeenCalledTimes(1);
+    expect(changeHandler).toHaveBeenCalledTimes(1);
+    expect(blurHandler).toHaveBeenCalledTimes(1);
+  });
+
+});
