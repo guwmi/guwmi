e3f7e220b53d37cc7eae9b26671be85e781fbea6
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 11:31:32 -0500

    cleans up code for the icon button compoent

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -2,36 +2,45 @@
 import React, { PropsWithChildren } from 'react';
 
 // component type
-interface ComponentProps extends PropsWithChildren {
+export interface IconButtonProps extends PropsWithChildren {
   size?: 'sm' | 'md' | 'lg';
   color?: 'primary' | 'secondary' | 'danger';
   variant?: 'fill' | 'outline' | 'ghost';
   theme?: 'square' | 'round' | 'circle';
-  className?: string;
-  onClick?: (React.MouseEventHandler<HTMLButtonElement> | undefined);
   ariaLabel: string;
   disabled?: boolean;
+  className?: string;
   skeleton?: boolean;
+  onClick?: (React.MouseEventHandler<HTMLButtonElement> | undefined);
 }
 
 /**
- * Icon Button Component ********************************************************************************
- * @param ComponentProps
+ * Icon Button component ********************************************************************
+ * 
+ * @param size - (options) size value of 'sm', 'md', or 'lg' - defaults to 'md'
+ * @param color - (optional) color value of 'primary', 'secondary', or 'danger' - defaults to 'primary'
+ * @param variant - (optional) variant value of 'fill', 'outline', or 'ghost' - defaults to 'fill'
+ * @param theme - (optional) theme value of 'square', 'round', or 'pill' - defaults to 'round'
+ * @param ariaLabel - string value for the aria-label property of the <button>> element
+ * @param disabled - (optionsl) boolean value that determines the disabled state of a link
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onClick - (optional) function to be called onClick
  */
 
-export default function IconButton(props: ComponentProps) {
+export default function IconButton(props: IconButtonProps) {
 
   const {
-    children,
     size = 'md',
     color = 'primary',
     variant = 'fill',
     theme = 'round',
-    className,
-    onClick,
     ariaLabel,
     disabled,
+    className,
     skeleton,
+    onClick,
+    children,
     ...rest
   } = props;
   const classes = `guwmi-btn icon ${size} ${color} ${variant} ${theme}${skeleton ? ' guwmi-skeleton' : ''}${className ? ' ' + className : ''}`;
