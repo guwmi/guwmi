2df34ef68a961a654b5659dec00be4c48179e1a4
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 14:53:46 -0500

    cleans up code for the checkbox component

--- src/components/Inputs/Checkbox/Checkbox.tsx
+++ src/components/Inputs/Checkbox/Checkbox.tsx
@@ -5,8 +5,8 @@ import { useId } from 'react';
 import Icon from '../../../components/Icon/Icon';
 
 // component type
-interface ComponentProps {
-  label?: string;
+export interface CheckboxProps {
+  label: string;
   id?: string;
   name?: string;
   disabled?: boolean;
@@ -19,15 +19,32 @@ interface ComponentProps {
   onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export default function Checkbox(props: ComponentProps) {
+/**
+ * Checkbox component ***********************************************************************
+ * 
+ * @param label - string value for the input label
+ * @param id - (optional) string value for the id proprty on the input
+ * @param name - (optional) string value for the name proprty on the input
+ * @param disabled - (optional) boolean value for the disabled state of the input
+ * @param value - (optional) string value for the value proprty on the input
+ * @param readOnly - (optional) boolean value for the readOnly state of the input
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onChange - (optional) function to be called when the input value changes
+ * @param onBlur - (optional) function to be called when focus leaves the input
+ * @param onFocus - (optional) function to be called when the input recieves focus
+ * 
+ */
+
+export default function Checkbox(props: CheckboxProps) {
 
   const {
     label,
     id,
     name,
-    disabled = false,
+    disabled,
     value,
-    readOnly = false,
+    readOnly,
     className,
     skeleton,
     onChange,
