5f276a786b86768a39a217ce5e7d07c52c117094
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 15:07:58 -0500

    cleans up code for the radio group component

--- src/components/Inputs/RadioGroup/RadioGroup.tsx
+++ src/components/Inputs/RadioGroup/RadioGroup.tsx
@@ -2,7 +2,7 @@
 import { useId } from 'react';
 
 // component type
-interface ComponentProps {
+interface RadioGroupProps {
   label: string;
   id?: string;
   name?: string;
@@ -19,7 +19,29 @@ interface ComponentProps {
   onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export default function RadioGroup(props: ComponentProps) {
+/**
+ * Radio Group component ********************************************************************
+ * 
+ * @param label - string value for the input label
+ * @param id - (optional) string value for the id proprty on the input
+ * @param name - (optional) string value for the name proprty on the input
+ * @param disabled - (optional) boolean value for the disabled state of the input
+ * @param value - (optional) string value for the value proprty on the input
+ * @param error - (optional) string vlaue to be displayed if the hasError prop is true
+ * @param hasError - (optional) boolean value to determine is the input should show as having an error
+ * @param options - array of radio group option objects:
+ *  - name - string value to be displayed for the option
+ *  - value - string value for the value property of te option
+ *  - disabled - (optional) boolean value to determine the options disabled state
+ * @param layout - (optional) value of 'column'or 'row' determines the layout of the radio options - defaults to 'column'
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onChange - (optional) function to be called when the input value changes
+ * @param onBlur - (optional) function to be called when focus leaves the input
+ * @param onFocus - (optional) function to be called when the input recieves focus
+ */
+
+export default function RadioGroup(props: RadioGroupProps) {
 
   const {
     label,
