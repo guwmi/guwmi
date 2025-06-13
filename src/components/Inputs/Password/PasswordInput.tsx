7a5faa92a78828b787646f34549fb0d24db8e3f8
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 14:59:37 -0500

    cleans up code for the password input component

--- src/components/Inputs/Password/PasswordInput.tsx
+++ src/components/Inputs/Password/PasswordInput.tsx
@@ -1,12 +1,14 @@
 // import library functionality
 import { useId, useRef, useState } from 'react';
 
-// import icon
-import Icon from '../../../components/Icon/Icon';
+// import custom functionality
 import useClickOutside from '../../../hooks/useClickOutside';
 
+// import components
+import Icon from '../../../components/Icon/Icon';
+
 // component type
-interface ComponentProps {
+export interface PasswordInputProps {
   label: string;
   hideLabel?: boolean;
   id?: string;
@@ -25,7 +27,29 @@ interface ComponentProps {
   onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export default function PasswordInput(props: ComponentProps) {
+/**
+ * Password input component *****************************************************************
+ * 
+ * @param label - string value for the input label
+ * @param hideLabel - (optional) boolean value that determines if the label is visibly hidden
+ * @param id - (optional) string value for the id proprty on the input
+ * @param name - (optional) string value for the name proprty on the input
+ * @param placeholder - (optional) string value for the placholder property on the input
+ * @param disabled - (optional) boolean value for the disabled state of the input
+ * @param value - (optional) string value for the value proprty on the input
+ * @param readOnly - (optional) boolean value for the readOnly state of the input
+ * @param error - (optional) string vlaue to be displayed if the hasError prop is true
+ * @param hasError - (optional) boolean value to determine is the input should show as having an error
+ * @param maxLength - (optional) number value for the maxLength proprty of the input
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onChange - (optional) function to be called when the input value changes
+ * @param onBlur - (optional) function to be called when focus leaves the input
+ * @param onFocus - (optional) function to be called when the input recieves focus
+ * 
+ */
+
+export default function PasswordInput(props: PasswordInputProps) {
 
   const {
     label,
@@ -50,6 +74,7 @@ export default function PasswordInput(props: ComponentProps) {
   const passwordRef = useRef<HTMLDivElement>(null);
   const classes = `guwmi-password-input${hasError ? ' error' : ''}${disabled ? ' disabled' : ''}${className ? ' ' + className : ''}`;
   const [type, setType] = useState<'text' | 'password'>('password');
+
   const toggleType = () => {
     if (type === 'password') {
       setType('text');
@@ -57,6 +82,7 @@ export default function PasswordInput(props: ComponentProps) {
       setType('password')
     }
   }
+  
   useClickOutside( type === 'text', toggleType, passwordRef );
 
   return (
