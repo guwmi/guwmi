965c177a15b0a909447b2f98a7f6300e94e59622
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 15:17:57 -0500

    cleans up code for text input compoent

--- src/components/Inputs/Text/TextInput.tsx
+++ src/components/Inputs/Text/TextInput.tsx
@@ -2,7 +2,7 @@
 import { useId } from 'react';
 
 // component type
-interface ComponentProps {
+export interface TextInputProps {
   label: string;
   hideLabel?: boolean;
   id?: string;
@@ -21,7 +21,29 @@ interface ComponentProps {
   onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
-export default function TextInput(props: ComponentProps) {
+/**
+ * Text Input component *********************************************************************
+ * 
+ * @param label - string value for the input label
+ * @param hideLabel - (optional) boolean value that determines if the label is visibly hidden
+ * @param id - (optional) string value for the id proprty on the input
+ * @param name - (optional) string value for the name proprty on the input
+ * @param placeholder - (optional) string value for the placholder property on the input
+ * @param disabled - (optional) boolean value for the disabled state of the input
+ * @param value - (optional) string value for the value proprty on the input
+ * @param readOnly - (optional) boolean value for the readOnly state of the input
+ * @param error - (optional) string vlaue to be displayed if the hasError prop is true
+ * @param hasError - (optional) boolean value to determine is the input should show as having an error
+ * @param maxLength - (optional) number value for the maxLength proprty of the input
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onChange - (optional) function to be called when the input value changes
+ * @param onBlur - (optional) function to be called when focus leaves the input
+ * @param onFocus - (optional) function to be called when the input recieves focus
+ * 
+ */
+
+export default function TextInput(props: TextInputProps) {
 
   const {
     label,
