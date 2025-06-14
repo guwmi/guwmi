3ff20a502637fe1bd9d4102e0f67be47baeefb4d
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 19:23:48 -0500

    cleans up the pagination component

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,12 +1,17 @@
 // import library functionality
-import { useEffect, useMemo, useState, useRef } from 'react';
+import {
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react';
 
 // import components
 import SelectInput from '../../components/Inputs/Select/SelectInput';
 import Icon from '../../components/Icon/Icon';
 
 // compoent type
-interface ComponentProps {
+export interface PaginationProps {
   currentPage?: number;
   currentSize: number;
   pageSizes: number[];
@@ -16,7 +21,21 @@ interface ComponentProps {
   onChange: (value: {currentPage: number, currentSize: number}) => void;
 }
 
-export default function Pagination(props: ComponentProps) {
+/**
+ * Pagination component ********************************************************************
+ * 
+ * @param currentPage - (optional) number value for the current page - defaults to 1
+ * @param currentSize - number value for the current page size
+ * @param pageSizes - array of number values for the page size select input
+ * @param totalItems - number value for the total number of items to paginate
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onChange - function to be called on any change event for the component
+ * * passes a value parameter of {currentPage: number, currentSize: number}
+ * 
+ */
+
+export default function Pagination(props: PaginationProps) {
 
   const {
     currentPage = 1,
@@ -33,20 +52,22 @@ export default function Pagination(props: ComponentProps) {
   const pageRef = useRef<number>(currentPage);
   const sizeRef = useRef<number>(currentSize);
   const classes = `guwmi-pagination${skeleton ? ' guwmi-skeleton' : ''}${className ? ' ' + className : ''}`;
+  const numPages = useMemo(() => Math.ceil(totalItems / size), [totalItems, size]);
+  const start = useMemo(() => (size * page) - size + 1, [size, page]);
+  const end = useMemo(() => (size * page) - size + size, [size, page]);
+
   const pageSizeArray = useMemo(() => {
     const options = pageSizes.filter((s) => s < totalItems)
       .map((s) => ({ name: s.toString(), value: s.toString() }));
     options.push({name: 'All', value: totalItems.toString()});
     return options;
   }, [pageSizes, totalItems]);
-  const numPages = useMemo(() => Math.ceil(totalItems / size), [totalItems, size]);
+
   const pageArray = useMemo(() => {
     return [...Array(Math.ceil(totalItems / size)).keys()].map(i => {
       return {name: (i + 1).toString(), value: (i + 1).toString()}
     })
   }, [totalItems, size]);
-  const start = useMemo(() => (size * page) - size + 1, [size, page]);
-  const end = useMemo(() => (size * page) - size + size, [size, page]);
 
   useEffect(() => {
     if (size !== sizeRef.current || page !== pageRef.current) {
