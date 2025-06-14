fc08f5fc572d1aa26098d14980632720805c35a5
Rustin Dodd <rustindodd@gmail.com>
2025-06-13 20:11:26 -0500

    cleans up code for the tag component

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -5,14 +5,37 @@ import { useRef } from 'react';
 interface ComponentProps {
   value: string;
   size?: 'sm' | 'md' | 'lg';
-  variant?: 'primary' | 'secondary' | 'outline' | 'neutral';
-  className?: string;
-  onClick?: (React.MouseEventHandler<HTMLButtonElement> | undefined);
+  variant?:
+  | 'primary'
+  | 'secondary'
+  | 'outline'
+  | 'neutral';
   href?: string;
-  target?: '_blank' | '_self' | '_parent' | '_top';
+  target?:
+  | '_blank'
+  | '_self'
+  | '_parent'
+  | '_top';
   disabled?: boolean;
+  className?: string;
   skeleton?: boolean;
+  onClick?: (React.MouseEventHandler<HTMLButtonElement> | undefined);
 }
+
+/**
+ * Tag component ****************************************************************************
+ * 
+ * @param value - string value for the text of the tag
+ * @param size - (options) size value of 'sm', 'md', or 'lg' - defaults to 'md'
+ * @param variant - (optional) color value of 'primary', 'secondary', 'outline', 'neutral' - defaults to 'neutral'
+ * @param href - (optional) string value for the href property of a link tag
+ * @param target - (optional) target value of '_blank', '_self', '_parent', or '_top' - only applies to buttons with an href property
+ * @param disabled - (optionsl) boolean value that determines the disabled state of a tag
+ * @param className - (optional) string value of class names to apply to the component
+ * @param skeleton - (optional) boolean vaule for whether the component should display as a skeleton
+ * @param onClick - (optional) function to be called onClick
+ * 
+ */
 
 export default function Tag(props: ComponentProps) {
 
@@ -20,16 +43,17 @@ export default function Tag(props: ComponentProps) {
     value,
     size = 'md',
     variant = 'neutral',
-    className,
-    onClick = null,
     href,
-    target = "_self",
+    target,
     disabled,
+    className,
     skeleton,
+    onClick = null,
     ...rest
   } = props;
   const classes = `guwmi-tag ${size} ${variant}${skeleton ? ' guwmi-skeleton' : ''}${className ? ' ' + className : ''}`;
   const button = useRef(null);
+  
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     button.current.focus();
     if (onClick) {
