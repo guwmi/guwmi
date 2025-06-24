b6461a7ce6c79eece850068a3f71214ba0e48d53
Rustin Dodd <rustindodd@gmail.com>
2025-06-24 09:24:26 -0500

    updates table styles

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ var __objRest = (source, exclude) => {
 };
 
 // src/index.ts
-import "./guwmi-32BOVQET.css";
+import "./guwmi-3CXRQBEO.css";
 
 // src/components/Accordion/Accordion.tsx
 import { useState, useEffect } from "react";
